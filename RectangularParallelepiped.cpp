29dbc62565000a6907240c0dad64394e28162b30
Takanori Oonuma <ohnhistory@yahoo.co.jp>
2013-06-09 09:14:37 +0900

    RectangularParallelepipedの作成
    直方体クラスを作り、DrawObjectの大まかな大きさを知ることができる。
    このクラスにisHit関数を作って大まかな当たり判定をするのにも悪くはないが、
    角度が入った場合の問題は依然として解決していない。
    
    Signed-off-by: Takanori Oonuma <ohnhistory@yahoo.co.jp>

---
+++ RectangularParallelepiped.cpp
@@ -0,0 +1,11 @@
+#include "RectangularParallelepiped.h"
+
+RectangularParallelepiped::RectangularParallelepiped()
+	: width(0.0), length(0.0), height(0.0)
+{
+}
+
+RectangularParallelepiped::RectangularParallelepiped(double length, double width, double height)
+	: length(length), width(width), height(height)
+{
+}
---
+++ RectangularParallelepiped.h
@@ -0,0 +1,22 @@
+#ifndef ___Class_RectangularParallelepiped
+#define ___Class_RectangularParallelepiped
+
+// ===== ’¼•û‘ÌƒNƒ‰ƒX ===== //
+class RectangularParallelepiped{
+
+public:
+	double length;
+	double width;
+	double height;
+
+	RectangularParallelepiped();
+	RectangularParallelepiped(double length, double width, double height);
+
+	void setRectangularParallelepiped(double length, double width, double height){
+		this->length;
+		this->width;
+		this->height;
+	}
+};
+
+#endif
