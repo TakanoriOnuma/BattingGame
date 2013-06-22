b82b61f17c953fdf3f8e152ffd7ebf3907a8c3eb
Takanori Onuma <ohnhistory@yahoo.co.jp>
2013-06-22 12:14:36 +0900

    TriFunctionの追加
    外部で作ったTriFunctionを追加。
    本来ならmasterでcommitするべきなのだが、一々切り替えるのも面倒で、
    どうせこのブランチもすぐにマージするからこっちにcommitすることに。
    
    Signed-off-by: Takanori Onuma <ohnhistory@yahoo.co.jp>

---
+++ TriFunction.cpp
@@ -0,0 +1,64 @@
+#include "TriFunction.h"
+
+#include <cmath>
+
+const double TriFunction::PI = 3.14159265358979323846;
+
+TriFunction::TriFunction()
+{
+	for(int i = 0; i < 360; i++){
+		sin_data[i] = ::sin(DegToRad(i));
+		cos_data[i] = ::cos(DegToRad(i));
+	}
+}
+
+// ŠÖ”‚É“à•”•Ï”‚ðŽ‚½‚¹‚ÄƒVƒ“ƒOƒ‹ƒgƒ“‚ðŽÀ‘•‚·‚é
+TriFunction& TriFunction::Instance()
+{
+	static TriFunction func;
+	return func;
+}
+
+int TriFunction::adj_degree(int degree)
+{
+	if(degree < 0){
+		degree *= -1;
+	}
+
+	while(degree > 360){
+		degree -= 360;
+	}
+	return degree;
+}
+
+double TriFunction::t_sin(int degree)
+{
+	return sin_data[adj_degree(degree)];
+}
+
+double TriFunction::t_cos(int degree)
+{
+	return cos_data[adj_degree(degree)];
+}
+
+double TriFunction::t_tan(int degree)
+{
+	int deg = adj_degree(degree);
+	return sin_data[deg] / cos_data[deg];
+}
+
+
+double TriFunction::sin(double degree)
+{
+	return ::sin(DegToRad(degree));
+}
+
+double TriFunction::cos(double degree)
+{
+	return ::cos(DegToRad(degree));
+}
+
+double TriFunction::tan(double degree)
+{
+	return ::tan(DegToRad(degree));
+}
---
+++ TriFunction.h
@@ -0,0 +1,36 @@
+#ifndef ___Class_TriFunction
+#define ___Class_TriFunction
+
+
+
+
+class TriFunction{
+	double sin_data[360];
+	double cos_data[360];
+
+	TriFunction();
+	TriFunction(const TriFunction&);
+	int adj_degree(int degree);
+public:
+	static const double PI;
+
+	static TriFunction& Instance();
+
+	double DegToRad(double degree){
+		return degree * PI / 180.0;
+	}
+	double RadToDeg(double radian){
+		return 180.0 * radian / PI;
+	}
+
+	double t_sin(int degree);
+	double t_cos(int degree);
+	double t_tan(int degree);
+
+	double sin(double degree);
+	double cos(double degree);
+	double tan(double degree);
+};
+
+
+#endif
