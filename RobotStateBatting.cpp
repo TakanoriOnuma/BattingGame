1b0ae4b0edac8ce49806b95dcbb152483a61d9f2
Takanori Onuma <ohnhistory@yahoo.co.jp>
2013-07-07 13:48:57 +0900

    それとない素振り
    それとなくバットを振っているように見せた。
    まだちゃんとはしていない。
    
    Signed-off-by: Takanori Onuma <ohnhistory@yahoo.co.jp>

--- RobotStateBatting.cpp
+++ RobotStateBatting.cpp
@@ -27,20 +27,23 @@ void BattingRobot::Batting::init(MyRobot& robot) const
 	double accel_vec = (8.0 + 4.0 * sqrt(5.0)) / (SWING_FRAME * SWING_FRAME);
 	double vec = -2.0 * (1.0 + sqrt(5.0)) / SWING_FRAME;
 
+	//double accel_vec = 8.0 / (SWING_FRAME * SWING_FRAME);
+	//double vec = -4.0 / SWING_FRAME;
+
 	battingRobot.accel_vec_r = accel_vec * angle;
-	battingRobot.vec_r = vec * angle;
+	battingRobot.vec_r = 180.0 / SWING_FRAME;
 
 	battingRobot.accel_vec_dis.x = 0.0;
 	battingRobot.accel_vec_dis.y = 0.0;
-	battingRobot.accel_vec_dis.z = -accel_vec * distance;
+	battingRobot.accel_vec_dis.z = 0.0;
 
 	battingRobot.vec_dis.x = 0.0;
 	battingRobot.vec_dis.y = 0.0;
-	battingRobot.vec_dis.z = -vec * distance;
+	battingRobot.vec_dis.z = -2 * distance / SWING_FRAME;
 	
 
 	double angle2 = 20.0;
-	double frame_time = SWING_FRAME / 3.0;
+	double frame_time = SWING_FRAME;
 	battingRobot.accel_vec_r2 = -8.0 * angle2 / (frame_time * frame_time);
 	battingRobot.vec_r2 = 4.0 * angle2 / frame_time;
 	
@@ -48,17 +51,27 @@ void BattingRobot::Batting::init(MyRobot& robot) const
 	battingRobot.bodyParts->rightArm->setBox2Angle(-90.0);
 
 
-	battingRobot.bodyParts->leftArm->setAngle(-20.0);
-	battingRobot.bodyParts->rightArm->setAngle(20.0);
+	battingRobot.bodyParts->leftArm->setAngle(-20.0 - 90.0);
+	battingRobot.bodyParts->rightArm->setAngle(20.0 - 90.0);
 
+	{
+		const double angle = 50.0;
+		const double frame_time = SWING_FRAME;
+		const double accel_angle_vec = -8.0 / (frame_time * frame_time);
+		const double angle_vec = 4.0 / frame_time;
+		battingRobot.bodyParts->leftArm->setBox2AngleAccelVector(accel_angle_vec * angle);
+		battingRobot.bodyParts->leftArm->setBox2AngleVector(angle_vec * angle);
+		battingRobot.bodyParts->rightArm->setBox2AngleAccelVector(accel_angle_vec * angle);
+		battingRobot.bodyParts->rightArm->setBox2AngleVector(angle_vec * angle);
+	}
 
 		// 所定の位置に戻る
 		Point3d pt = battingRobot.bodyParts->leftArm->getPoint();
-		pt.z = 0.0;
+		pt.z = distance;
 		battingRobot.bodyParts->leftArm->move(pt);
 		pt.x = battingRobot.bodyParts->rightArm->getPoint().x;
+		pt.z = -distance;
 		battingRobot.bodyParts->rightArm->move(pt);
-
 }
 
 void BattingRobot::Batting::update(MyRobot& robot) const
@@ -67,30 +80,20 @@ void BattingRobot::Batting::update(MyRobot& robot) const
 	// (BattingRobotクラスしかBattingクラスを知らないため)
 	BattingRobot& battingRobot = dynamic_cast<BattingRobot&>(robot);
 
+
+
 	battingRobot.bodyParts->leftArm->addAngle(battingRobot.vec_r);
 	battingRobot.bodyParts->rightArm->addAngle(battingRobot.vec_r);
-	battingRobot.vec_r += battingRobot.accel_vec_r;
 
 	battingRobot.bodyParts->leftArm->update();
 	battingRobot.bodyParts->rightArm->update();
 
-	if(battingRobot.frame < SWING_FRAME - SWING_FRAME / 12){
-		battingRobot.bodyParts->leftArm->move(battingRobot.vec_dis);
-		battingRobot.bodyParts->rightArm->move(-battingRobot.vec_dis);
-	}
-	battingRobot.vec_dis += battingRobot.accel_vec_dis;
 
-	if(battingRobot.frame == SWING_FRAME / 3){
-		const double angle = 50.0;
-		const double frame_time = 2.0 * SWING_FRAME / 3.0;
-		const double accel_angle_vec = -8.0 / (frame_time * frame_time);
-		const double angle_vec = 4.0 / frame_time;
-		battingRobot.bodyParts->leftArm->setBox2AngleAccelVector(accel_angle_vec * angle);
-		battingRobot.bodyParts->leftArm->setBox2AngleVector(angle_vec * angle);
-		battingRobot.bodyParts->rightArm->setBox2AngleAccelVector(accel_angle_vec * angle);
-		battingRobot.bodyParts->rightArm->setBox2AngleVector(angle_vec * angle);
+	battingRobot.bodyParts->leftArm->move(battingRobot.vec_dis);
+	battingRobot.bodyParts->rightArm->move(-battingRobot.vec_dis);
 
-	}
+
+
 
 	if(battingRobot.frame <= SWING_FRAME / 3){
 		battingRobot.bodyParts->leftArm->addAngle(battingRobot.vec_r2);
