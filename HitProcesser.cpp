c7fe76231cd220f8682bf70faf2603a495e3438b
Takanori Onuma <ohnhistory@yahoo.co.jp>
2013-07-07 10:08:09 +0900

    Box1の座標までの移動
    バットまで移動するのはとても面倒だが、どうしてもそれ以外の方法は
    思い浮かばなかった。そのためゴリ押しでどうにかする。
    
    Signed-off-by: Takanori Onuma <ohnhistory@yahoo.co.jp>

--- HitProcesser.cpp
+++ HitProcesser.cpp
@@ -1,19 +1,43 @@
+#include <iostream>
+
 #include "HitProcesser.h"
 
 #include "MyRobot_BodyParts.h"
+#include "TriFunction.h"
+#include "KeyBoardManager.h"
 
 #include "MyLine.h"
 
+using namespace std;
+
+static void outPoint(string str, const Point3d& pt)
+{
+	cout << str << "(" << pt.x << ", " << pt.y << ", " << pt.z << ")" << endl;
+}
+
 void Game::HitProcesser::hitProcess(MyBall& ball, BattingRobot& battingRobot, MyLine& line)
 {
+	TriFunction& triFunc = TriFunction::getInstance();
+
 	Point3d bat_pt = battingRobot.getPoint();
 	const Point3d& arm_pt = battingRobot.getBodyParts().leftArm->getPoint();
+	const double arm_dis = arm_pt.x;		// arm_pt.z = 0‚È‚½‚ßarm_pt.x‚¾‚¯‚Å‹——£‚É‚È‚é
+	const double robot_angle = battingRobot.getAngle();
 
-	bat_pt.x += arm_pt.x;
+	bat_pt.x += arm_dis * triFunc.t_cos(robot_angle);
 	bat_pt.y += arm_pt.y;
-	bat_pt.z += arm_pt.z;
+	bat_pt.z += arm_dis * triFunc.t_sin(-robot_angle);
 
 	line.setPoint2(bat_pt.x, bat_pt.y, bat_pt.z);
+
+	if(KeyboardManager::getInstance().isPushCharKey(' ')){
+		const Point3d& robot_pt = battingRobot.getPoint();
+		Point3d box1_pt = battingRobot.getBodyParts().leftArm->getPoint();
+
+		outPoint("robot_pt", robot_pt);
+		outPoint("box1_pt", box1_pt);
+		outPoint("bat_pt", bat_pt);
+	}
 
 
 	// ƒ{[ƒ‹‚ªŒü‚©‚Á‚Ä‚­‚é•ûŒü‚ªˆá‚¤‚È‚ç
