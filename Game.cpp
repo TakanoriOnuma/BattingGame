c36b727cfdd1a312b0f9a3826f12b4daf3fdf118
Takanori Onuma <ohnhistory@yahoo.co.jp>
2013-07-17 02:00:10 +0900

    マージ問題の解決
    Signed-off-by: Takanori Onuma <ohnhistory@yahoo.co.jp>

--- Game.cpp
+++ Game.cpp
@@ -61,7 +61,6 @@ struct Game::DrawableObjects{
 		batting_field(0.0, -0.1, 3.0, 1.5, 1.5, ColorData(1.0, 0.0, 0.0)),
 		circle(0.0, 0.0, 3.0, 0.1, ColorData(0.0, 1.0, 0.0))
 	{
-<<<<<<< HEAD
 		Point3d move_pt;
 		move_pt.x = ground.getPoint().x;
 		move_pt.y = ground.getPoint().y + backWall.getRectBox().height;
@@ -69,11 +68,8 @@ struct Game::DrawableObjects{
 		backWall.move(move_pt);
 		backWall.setMaterialData(MaterialData::createMaterialData(Color::BLUE));
 		
-		ball.setMaterialData(MaterialData::createMaterialData(Jewel::OBSIDIAN));
-=======
 		ball.setDiffuse(1.0f, 1.0f, 1.0f);
 		ball.setAmbient(1.0f, 1.0f, 1.0f);
->>>>>>> Texture
 		battingRobot.setRotateVector(0.0, 1.0, 0.0);
 		battingRobot.setAngle(90.0);
 		battingRobot.setMaterialData(MaterialData::createMaterialData(Jewel::TURQUOISE));
