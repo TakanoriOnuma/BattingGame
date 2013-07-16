7c1fa9db35b33b05395a21353bfa36847fa308bd
Takanori Onuma <ohnhistory@yahoo.co.jp>
2013-07-17 00:26:28 +0900

    文字列の描画の座標の変更
    Signed-off-by: Takanori Onuma <ohnhistory@yahoo.co.jp>

--- GameDecorator.cpp
+++ GameDecorator.cpp
@@ -102,30 +102,30 @@ void GameDecorator::display() const
 
 	glDisable(GL_LIGHTING);
 	glColor3d(1.0, 0.0, 0.0);
-	glRasterPos3d(-3.0, 2.0, 0.0);
+	glRasterPos3d(-3.0, 2.2, 0.0);
 	drawString("Difficulity:");
 	drawString(game->difficulity_str.c_str());
 
 	if(game->hitProcesser == Game::RoughHitProcesser::getInstance()){
 		glColor3d(0.0, 1.0, 1.0);
-		glRasterPos3d(-3.0, 1.5, 0.0);
+		glRasterPos3d(-3.0, 1.7, 0.0);
 		drawString("RoughHitProcesser");
 	}
 	else if(game->hitProcesser == Game::NoDelayHitProcesser::getInstance()){
 		glColor3d(1.0, 0.0, 1.0);
-		glRasterPos3d(-3.0, 1.5, 0.0);
+		glRasterPos3d(-3.0, 1.7, 0.0);
 		drawString("NoDelayHitProcesser");
 	}
 
 	glColor3d(0.5, 0.0, 0.5);
-	glRasterPos3d(-3.0, 1.0, 0.0);
+	glRasterPos3d(-3.0, 1.2, 0.0);
 	drawString(game->usingDeviceName.c_str());
 
 	// ボールの残りの数を表示
 	stringstream stream;
 	stream << game->ball_num;
 	glColor3d(0.0, 0.0, 1.0);
-	glRasterPos3d(1.5, 2.0, 0.0);
+	glRasterPos3d(1.8, 2.0, 0.0);
 	drawString("Ball:");
 	drawString(stream.str().c_str());
 
@@ -133,13 +133,13 @@ void GameDecorator::display() const
 	stream.str("");		// バッファのクリア
 	stream << game->score;
 	glColor3d(0.0, 0.0, 1.0);
-	glRasterPos3d(1.5, 1.5, 0.0);
+	glRasterPos3d(1.8, 1.5, 0.0);
 	drawString("Score:");
 	drawString(stream.str().c_str());
 
 	// 結果の表示
 	glColor3d(1.0, 0.0, 1.0);
-	glRasterPos3d(1.7, 0.5, 0.0);
+	glRasterPos3d(1.9, 0.5, 0.0);
 	drawString(game->result_str.c_str());
 
 	glEnable(GL_LIGHTING);
